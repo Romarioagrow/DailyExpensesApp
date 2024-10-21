0fb7c7f2c79f440f1f185f3db3ae01b673c0bf46
Roman Moltanovski <Romarioagrow@mail.ru>
2024-10-22 04:43:48 +0500

    Purchase item swipeable

--- components/PurchaseItem.js
+++ components/PurchaseItem.js
@@ -1,36 +1,53 @@
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
-
+import { View, Text, StyleSheet } from 'react-native';
+import { RectButton, Swipeable } from 'react-native-gesture-handler';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 const PurchaseItem = ({ purchase, onDelete }) => {
+    // Компонент, который будет рендериться при свайпе влево
+    const renderRightActions = () => (
+      <RectButton style={styles.deleteButton} onPress={onDelete}>
+          <Text style={styles.deleteText}>Delete</Text>
+      </RectButton>
+    );
+
     return (
-      <View style={styles.item}>
-          <Icon name="shopping-cart" size={24} color="#333" />
-          <View style={styles.info}>
-              <Text style={styles.type}>{purchase.type}</Text>
-              <Text style={styles.description}>{purchase.description}</Text>
+      <Swipeable renderRightActions={renderRightActions}>
+          <View style={styles.itemWrapper}>
+              <View style={styles.item}>
+                  <Icon name="shopping-cart" size={24} color="#333" />
+                  <View style={styles.info}>
+                      <Text style={styles.type}>{purchase.type}</Text>
+                      <Text style={styles.description}>{purchase.description}</Text>
+                  </View>
+                  <Text style={styles.price}>${purchase.cost}</Text>
+              </View>
           </View>
-          <Text style={styles.price}>${purchase.cost}</Text>
-      </View>
+      </Swipeable>
     );
 };
 
 const styles = StyleSheet.create({
+    itemWrapper: {
+        marginHorizontal: 16, // Внешний отступ по бокам для тени
+        marginBottom: 16,     // Внешний отступ снизу для тени
+    },
     item: {
         flexDirection: 'row',
         justifyContent: 'space-between',
+        alignItems: 'center',
         padding: 18,
         backgroundColor: '#FFFFFF',
         borderRadius: 12,
-        marginBottom: 12,
         shadowColor: '#000',
         shadowOpacity: 0.1,
         shadowRadius: 10,
+        shadowOffset: { width: 0, height: 5 },
         elevation: 4,
     },
     info: {
         flex: 1,
+        marginLeft: 10,
     },
     type: {
         fontSize: 16,
@@ -49,11 +66,16 @@ const styles = StyleSheet.create({
     },
     deleteButton: {
         backgroundColor: '#FF3B30',
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 80,
         borderRadius: 8,
-        padding: 8,
+        marginVertical: 4,
     },
     deleteText: {
         color: '#FFF',
+        fontWeight: 'bold',
+        fontSize: 16,
     },
 });
 
